d874c374e4363a40bd5dd2207e4eac7911c23c32
Probirochniy <afl_damir@mail.ru>
2023-06-15 23:30:42 +0300

    linker2

--- links_creating.py
+++ links_creating.py
@@ -1,65 +1,45 @@
 # specify the directory path
 import os
 
+
+def get_names(old_dirs):
+    new_names = []
+    for dir in old_dirs:
+        dir = dir.split('''\\''')
+        list_splitted = dir[len(dir) - 1].split('-')
+        del list_splitted[0]
+        name = ''
+        for i in range(len(list_splitted)):
+            name += list_splitted[i]
+            if i != len(list_splitted) - 1:
+                 name += '-'
+        new_names.append(name)
+    return new_names
+
+
+def get_dirs(dir_paths, counter):
+    dirs = []
+    for dir_path in dir_paths:
+        files_and_dirs = os.listdir(dir_path)
+        for item in files_and_dirs:
+            item_path = os.path.join(dir_path, item)
+            if os.path.isdir(item_path):
+                dirs.append(item_path)
+    if counter == 0:
+        return dirs
+    else:
+        return get_dirs(dirs, counter-1)
+    
+
+def create_links(dir_paths):
+    
+    old_dirs = get_dirs(dir_paths, 1)
+    
+    new_names = get_names(old_dirs)
+    print(new_names)
+
+    for i in range(len(old_dirs)):
+        os.symlink(old_dirs[i], './tasks/' + new_names[i])
+
 dir_paths = ['./checkers/01-introduction', './checkers/02-bash-programming']
-
-names = []
-for dir_path in dir_paths:
-
-    files_and_dirs = os.listdir(dir_path)
-
-    for item in files_and_dirs:
-
-        item_path = os.path.join(dir_path, item)
-
-        if os.path.isdir(item_path):
-
-            sub_files_and_dirs = os.listdir(item_path)
-
-            for sub_item in sub_files_and_dirs:
-
-                sub_item_path = os.path.join(item_path, sub_item)
-
-                if os.path.isdir(sub_item_path):
-                    splitted = str(sub_item).split('-')
-                    name = ''
-                    for i in range(len(splitted)):
-                        if i != 0:
-                            name += splitted[i]
-                            if i != len(splitted) - 1:
-                                name += '-'
-                    names.append(name)
-
-
-print(names)
-
-# for folder_name in names:
-#     if not os.path.exists('./tasks/' + folder_name):
-#         os.makedirs('./tasks/' + folder_name)
-#         print(f"Folder {folder_name} has been created successfully!")
-#     else:
-#         print(f"Folder {folder_name} already exists!")
-
-counter = 0
-
-for dir_path in dir_paths:
-
-    files_and_dirs = os.listdir(dir_path)
-
-    for item in files_and_dirs:
-
-        item_path = os.path.join(dir_path, item)
-
-        if os.path.isdir(item_path):
-
-            sub_files_and_dirs = os.listdir(item_path)
-
-            for sub_item in sub_files_and_dirs:
-
-                sub_item_path = os.path.join(item_path, sub_item)
-
-                if os.path.isdir(sub_item_path):
-                    print(sub_item_path)
-                    print(names[counter])
-                    os.symlink(sub_item_path, './tasks/' + names[counter])
-                    counter += 1
+create_links(dir_paths)
