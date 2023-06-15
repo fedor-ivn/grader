589caaffaa3ee0315c5e3e89f633269f1975ea77
Probirochniy <afl_damir@mail.ru>
2023-06-15 21:27:58 +0300

    gitkeep

--- links_creating.py
+++ links_creating.py
@@ -1,33 +1,35 @@
 # specify the directory path
 import os
 
-dir_paths = ['./checkers/01-introduction', './checkers/02-bash-programming']
+dir_paths = ["./checkers/01-introduction", "./checkers/02-bash-programming"]
+
+
+class TasksDirectory:
+    def __init__(checkers_path="checkers", target_path="tasks"):
+        pass
+
 
 names = []
 for dir_path in dir_paths:
-
     files_and_dirs = os.listdir(dir_path)
 
     for item in files_and_dirs:
-
         item_path = os.path.join(dir_path, item)
 
         if os.path.isdir(item_path):
-
             sub_files_and_dirs = os.listdir(item_path)
 
             for sub_item in sub_files_and_dirs:
-
                 sub_item_path = os.path.join(item_path, sub_item)
 
                 if os.path.isdir(sub_item_path):
-                    splitted = str(sub_item).split('-')
-                    name = ''
+                    splitted = str(sub_item).split("-")
+                    name = ""
                     for i in range(len(splitted)):
                         if i != 0:
                             name += splitted[i]
                             if i != len(splitted) - 1:
-                                name += '-'
+                                name += "-"
                     names.append(name)
 
 
@@ -43,23 +45,19 @@ print(names)
 counter = 0
 
 for dir_path in dir_paths:
-
     files_and_dirs = os.listdir(dir_path)
 
     for item in files_and_dirs:
-
         item_path = os.path.join(dir_path, item)
 
         if os.path.isdir(item_path):
-
             sub_files_and_dirs = os.listdir(item_path)
 
             for sub_item in sub_files_and_dirs:
-
                 sub_item_path = os.path.join(item_path, sub_item)
 
                 if os.path.isdir(sub_item_path):
                     print(sub_item_path)
                     print(names[counter])
-                    os.symlink(sub_item_path, './tasks/' + names[counter])
+                    os.symlink(sub_item_path, "./tasks/" + names[counter])
                     counter += 1
